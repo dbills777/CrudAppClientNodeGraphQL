8818f16ddcabfe95b5b48465605601e17c3865c9
Derrick Bills <dbills777@gmail.com>
2021-04-22 15:40:27 -0600

    back to local

--- client/src/index.js
+++ client/src/index.js
@@ -3,27 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-// import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-// import { onError } from '@apollo/client/link/error';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 
-// const errorLink = onError(({ graphqlErrors, networkError }) => {
-//   if (graphqlErrors) {
-//     graphqlErrors.map(({ message, location, path }) => {
-//       alert(`Graphql error ${message}${location}${path}${networkError}`);
-//     });
-//   }
-// });
-// const link = from([errorLink, new HttpLink({ uri: '/graphql' })]);
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+const link = from([errorLink, new HttpLink({ uri: 'http://localhost:4000/graphql' })]);
 
-// const client = new ApolloClient({
-//   link: link,
-//   cache: new InMemoryCache(),
-// });
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: link,
   cache: new InMemoryCache(),
 });
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/graphql/',
+//   cache: new InMemoryCache(),
+// });
 
 ReactDOM.render(
   <React.StrictMode>
